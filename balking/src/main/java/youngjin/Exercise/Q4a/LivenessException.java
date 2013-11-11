61fa57a60f26123e408eb24d671b27593902113a
kenshin579 <kenshin579@hotmail.com>
2013-11-11 20:13:33 +0900

    Balking 패턴 Exercise 추가

---
+++ balking/src/main/java/youngjin/Exercise/Q4a/LivenessException.java
@@ -0,0 +1,7 @@
+package youngjin.Exercise.Q4a;
+
+public class LivenessException extends RuntimeException {
+    public LivenessException(String msg) {
+        super(msg);
+    }
+}
---
+++ balking/src/main/java/youngjin/Exercise/Q5/Main.java
@@ -0,0 +1,10 @@
+package youngjin.Exercise.Q5;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new TestThread();
+        while (true) {
+            thread.start();
+        }
+    }
+}
